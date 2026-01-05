80a381c304f3182c9822a6af22b2352a571591c8
Th3-Bak3r <aboumat01@gmail.com>
2026-01-05 21:25:28 +0100

    updated hero images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,13 @@ import BookingForm from './bookingForm';
 import { useEffect, useState } from 'react';
 
 const images = [
-  'https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1600&q=80',
-  'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1600&q=80',
-  'https://images.unsplash.com/photo-1504593811423-6dd665756598?auto=format&fit=crop&w=1600&q=80',
-  'https://images.unsplash.com/photo-1491553895911-0055eca6402d?auto=format&fit=crop&w=1600&q=80'
+  'https://imgs.search.brave.com/EM0_BWdjNis1g_7gqoCfExAkWhIeIqIG99IRhsxcqmk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLm5h/dGdlb2ZlLmNvbS9u/LzdlZGMwNTgxLTZl/OGEtNGJlOS05NjI4/LTViNDY4YThhZTQ0/MC95b3Vyc2hvdC1t/b3JvY2NvMDE4Lmpw/Zw',
+  'https://imgs.search.brave.com/Zxl3EgJWwI4IB8qR8qBMlcLkgoAQS5tRLyYyvxQXphM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI1/MDQyODU4OS9waG90/by9tb3JvY2NvLWNp/dHkuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWR0eDdvVE1L/Tms5b2RNbWV2U1B6/ZGhKNjg0bEtmN3pC/dkpCQU9tdXZTRVU9',
+  'https://imgs.search.brave.com/MyjrNXteqmjQM50PT9SuxyDKGDRiVI8kPSUpVp3Thgo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQ0/MDk4MTkzMi9waG90/by9mYW1vdXMtaGlz/dG9yaWMtaGFzc2Fu/LWlpLW1vc3F1ZS1p/bi1tYXJyYWtlY2gt/Y2l0eS1tb3JvY2Nv/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz1RbThCVDVRbjJ0/VEl0SU9WWFkyM1k5/cV9HNE1CYjNDVDBs/MnE5TlIwaWh3PQ',
+  'https://imgs.search.brave.com/V05414PfnJFQfYqpp5Xf2JEhZw_jw8F92HFZvlXQC80/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDk4/NDAyMDQ5L3Bob3Rv/L2phbWFhLWVsLWZu/YS1tYXJyYWtlc2gt/bW9yb2Njby5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9Y2E1/R1pWU2ZaSGZaTkRS/eW03WFYwS1hkN0E1/VkxmNld1NmlzZTNu/M3d2WT0',
+  'https://imgs.search.brave.com/5bHft4jUpF_XTUez2qqT9OtL-6lyCMYjvF6_QfA1zf8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMjE2/ODQzMDgyNS9waG90/by9jaGVmY2hhb3Vl/bi1tb3JvY2NvLndl/YnA_YT0xJmI9MSZz/PTYxMng2MTImdz0w/Jms9MjAmYz15N3d4/ZXVqSmh1OXhXbWRO/VGN1UkRzb2RPQ0R0/c0dNRklYVVV1Sms4/M2pBPQ',
+  'https://imgs.search.brave.com/llZn0uHp6iBYiTWJGpPyeElx_FnfZmzHwSqHMxJyWL8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9hZ2Fk/aXItbW9yb2Njby1h/ZnJpY2Eta2FzYmFo/LWFnYWRpci1vdWZs/YS1mb3J0LWF0bGFu/dGljLW9jZWFuLWNp/dHktc2t5bGluZS1w/YW5vcmFtaWMtYWVy/aWFsLXZpZXctbGlm/ZS1hZ2FkaXItbW9y/b2Njby0yNzQ4OTg2/NDkuanBn',
+  'https://imgs.search.brave.com/15hPnTarJbUKr3jW9sV2Ur5vnapqkXKeHngM7DFOyJk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cmV2aWdvcmF0ZS5j/b20vaW1hZ2VzL3Ro/ZS1iZXN0LWJlYWNo/LXJlc29ydC1kZXN0/aW5hdGlvbi1hZ2Fk/aXIuanBn'
 ];
 
 const Hero = () => {
