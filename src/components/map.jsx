5afdc29121b1209fc991b8eea912c45cf03de564
Th3-Bak3r <aboumat01@gmail.com>
2026-01-06 12:33:03 +0100

    updated the map location

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -3,7 +3,7 @@ function MapComponent() {
      <div style={{ width: "100%", height: "400px" }}>
          {/* use an embeddable maps URL (output=embed) instead of the normal maps page which blocks framing */}
          <iframe
-            src={`https://www.google.com/maps?q=30.403174,-9.5285335&z=14&output=embed`}
+            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2719.349552406364!2d-9.71112031647218!3d30.543869714475846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdb24d2681cae4f1%3A0xa12365a36efbb31c!2sTaghazout%20Beach!5e1!3m2!1sen!2sma!4v1767699109029!5m2!1sen!2sma`}
             width="100%"
             height="100%"
             style={{ border: 0 }}
@@ -16,3 +16,4 @@ function MapComponent() {
          } 
          
          export default MapComponent;
+
